4c1cfb81a5eeefec58aaf39d1cf81f8b89b297b1
LungRaul <62697969+LungRaul@users.noreply.github.com>
2020-11-06 10:50:19 +0200

    Add files via upload

---
+++ Lung Raul/L06/Profile.Domain/CreateQuestionWorkflow/VerifyQuestionService.cs
@@ -0,0 +1,17 @@
+﻿using LanguageExt.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Question.Domain.CreateQuestionWorkflow.QuestionText;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    public class VerifyQuestionService
+    {
+        public Result<VerifiedQuestion> VerifyQuestion(UnverifiedQuestion question)
+        {
+            // se publica intrebarea doar daca se respecta conditiile de 1000 caractere si intre 1 si 3 taguri
+            return new VerifiedQuestion(question.Question,question.Tags);
+        }
+    }
+}
---
+++ Lung Raul/L06/Profile.Domain/CreateQuestionWorkflow/VerifyVoteService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static Question.Domain.CreateQuestionWorkflow.QuestionText;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    public class VerifyVoteService
+    {
+        public Task VerifyIfQuestionIsPosted(VerifiedQuestion question)
+        {
+            //aici lipseste logica prin care se verifica ca intrebarea e postata.Daca nu e postata nu poti vota
+            return Task.CompletedTask;
+        }
+     }
+}
